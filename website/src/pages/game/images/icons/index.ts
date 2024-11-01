68b52321b9f516dcafd1ef7c217ede1cb0e1152d
danielhong <dan.hong378@gmail.com>
2024-11-01 21:41:19 +1100

    port over

---
+++ website/src/pages/game/images/icons/index.ts
@@ -0,0 +1,5 @@
+import icon_steam from './icon_steam.png';
+import icon_twitter from './icon_twitter.png';
+import icon_youtube from './icon_youtube.png';
+
+export { icon_steam, icon_twitter, icon_youtube };
---
+++ website/src/pages/game/images/index.ts
@@ -0,0 +1,14 @@
+import * as icons from './icons';
+import sw_screen from './sw_screen.png';
+import sw_screen10 from './sw_screen10.png';
+import sw_screen11 from './sw_screen11.png';
+import sw_screen12 from './sw_screen12.png';
+import sw_screen15 from './sw_screen15.png';
+import sw_screen4 from './sw_screen4.png';
+import sw_screen5 from './sw_screen5.png';
+import sw_screen7 from './sw_screen7.png';
+import sw_screen9 from './sw_screen9.png';
+import walking_character from './walking_character.jpg';
+
+export { icons, sw_screen, sw_screen10, sw_screen11, sw_screen12, sw_screen15, sw_screen4, sw_screen5, sw_screen7, sw_screen9, walking_character };
+
